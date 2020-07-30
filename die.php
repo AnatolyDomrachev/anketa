52201cb8ba62fa5d7a75ff9291663cc63aeca1d0
Ваше Имя <you@example.com>
2020-07-30 09:26:11 +0300

    1

---
+++ die.php
@@ -0,0 +1,16 @@
+<table>
+<tr>
+ <td>
+ Дата смерти 
+<td>
+ <input type=text name = rod[<?php echo $_GET['type'] ?>][data_s][value]>
+ <td>
+ <input type=checkbox name = rod[<?php echo $_GET['type'] ?>][data_s][netochno]>
+ <tr>
+ <td>
+ Место смерти
+<td>
+ <input type=text name = rod[<?php echo $_GET['type'] ?>][mesto_s][value]>
+ <td>
+ <input type=checkbox name = rod[<?php echo $_GET['type'] ?>][mesto_s][netochno]>
+ </table>
