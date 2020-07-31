2abaf059c5526a87e21a015aaeb59a38a6f1f272
Ваше Имя <you@example.com>
2020-07-31 16:55:03 +0300

    1

--- add_brak.php
+++ add_brak.php
@@ -1,4 +1,85 @@
-<button  type="button" onclick="add_pred_brak('papa');">
+<?php 
+$num = $_GET['num'];;
+$rod = $_GET['type'];
+$id = 'add_brak_'.$rod.$num;
+?>
+
+<?php if( $num == 0  ): ?>
+
+<b>
+Сведения о браке:
+</b>
+	<p>
+<table>
+<tr>
+<td>
+	Дата заключения брака
+<td>
+<input type=text name = rod[papa][brak][<?php echo $num; ?>][data_b][value]>
+<td>
+<input type=checkbox name = rod[papa][brak][<?php echo $num; ?>][data_b][netochno]>
+<tr>
+<td>
+	Место заключения брака
+<td>
+<input type=text name = rod[papa][brak][<?php echo $num; ?>][mesto_b][value]>
+<td>
+<input type=checkbox name = rod[papa][brak][<?php echo $num; ?>][mesto_b][netochno]>
+
+<tr>
+<td>
+Дата расторжения брака
+	<br>
+(если был расторгнут)
+<td>
+<input type=text name = rod[papa][brak][<?php echo $num; ?>][data_rast_b][value]>
+<td>
+<input type=checkbox name = rod[papa][brak][<?php echo $num; ?>][data_rast_b][netochno]>
+
+</table>
+
+<?php endif; ?>
+
+<?php if( $num != 0  ): ?>
+<p>
+<table>
+<tr>
+<td>
+ФИО бывшей супруги 
+<td>
+<input type=text name = rod[papa][brak][<?php echo $num; ?>][fio][value]>
+<td>
+<input type=checkbox name = rod[papa][brak][<?php echo $num; ?>][fio][netochno]>
+<tr>
+<td>
+Дата заключения брака 
+<td>
+<input type=text name = rod[papa][brak][<?php echo $num; ?>][data][value]>
+<td>
+<input type=checkbox name = rod[papa][brak][<?php echo $num; ?>][data][netochno]>
+<tr>
+<td>
+Место заключения брака 
+<td>
+<input type=text name = rod[papa][brak][<?php echo $num; ?>][mesto][value]>
+<td>
+<input type=checkbox name = rod[papa][brak][<?php echo $num; ?>][mesto][netochno]>
+<tr>
+<td>
+Дата расторжения брака 
+<td>
+<input type=text name = rod[papa][brak][<?php echo $num; ?>][data_r][value]>
+<td>
+<input type=checkbox name = rod[papa][brak][<?php echo $num; ?>][data_r][netochno]>
+<tr>
+<td>
+</table>
+<?php endif; ?>
+
+</div>
+<div class = "<?php echo $id; ?>" id = "<?php echo $id; ?>" >
+
+<button  type="button" onclick="ftc('add_brak.php?type=<?php echo $rod; ?>&num=<?php echo $num+1; ?>', '<?php echo $id; ?>');">
 	добавить сведения о предыдущих браках
 </button>
 
--- brak.php
+++ brak.php
@@ -1,3 +1,5 @@
+<?php if() ?>
+
 <b>
 Сведения о браке:
 </b>
@@ -7,26 +9,26 @@
 <td>
 	Дата заключения брака
 <td>
-<input type=text name = rod[papa][brak][data_b][value]>
+<input type=text name = rod[papa][brak][0][data_b][value]>
 <td>
-<input type=checkbox name = rod[papa][brak][data_b][netochno]>
+<input type=checkbox name = rod[papa][brak][0][data_b][netochno]>
 <tr>
 <td>
 	Место заключения брака
 <td>
-<input type=text name = rod[papa][brak][mesto_b][value]>
+<input type=text name = rod[papa][brak][0][mesto_b][value]>
 <td>
-<input type=checkbox name = rod[papa][brak][mesto_b][netochno]>
+<input type=checkbox name = rod[papa][brak][0][mesto_b][netochno]>
 
 <tr>
 <td>
-	Дата расторжения
+Дата расторжения брака
 	<br>
-	(если был расторгнут)
+(если был расторгнут)
 <td>
-<input type=text name = rod[papa][brak][data_rast_b][value]>
+<input type=text name = rod[papa][brak][0][data_rast_b][value]>
 <td>
-<input type=checkbox name = rod[papa][brak][data_rast_b][netochno]>
+<input type=checkbox name = rod[papa][brak][0][data_rast_b][netochno]>
 
 </table>
 
@@ -36,4 +38,44 @@
 
 <p>
 
+<?php endif ?>
 
+<?php if() ?>
+
+<table>
+<tr>
+<td>
+ФИО бывшей супруги 
+<td>
+<input type=text name = rod[papa][brak][1][fio][value]>
+<td>
+<input type=checkbox name = rod[papa][brak][1][fio][netochno]>
+<tr>
+<td>
+Дата заключения брака 
+<td>
+<input type=text name = rod[papa][brak][1][data][value]>
+<td>
+<input type=checkbox name = rod[papa][brak][1][data][netochno]>
+<tr>
+<td>
+Место заключения брака 
+<td>
+<input type=text name = rod[papa][brak][1][mesto][value]>
+<td>
+<input type=checkbox name = rod[papa][brak][1][mesto][netochno]>
+<tr>
+<td>
+Дата расторжения брака 
+<td>
+<input type=text name = rod[papa][brak][1][data_r][value]>
+<td>
+<input type=checkbox name = rod[papa][brak][1][data_r][netochno]>
+<tr>
+<td>
+
+<button  type="button" onclick="ftc('add_rab.php?type=<?php echo $rod; ?>&nrab=<?php echo $nrab+1; ?>', '<?php echo $id; ?>');">
+	добавить сведения о предыдущих браках
+</button>
+
+<?php endif ?>
