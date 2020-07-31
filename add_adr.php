afb19582932e307caf2787bb3cf104d3a81d5035
Ваше Имя <you@example.com>
2020-07-31 09:08:18 +0300

    1

---
+++ add_adr.php
@@ -0,0 +1,49 @@
+<?php 
+$nadr = $_GET['nadr'];;
+$rod = $_GET['type'];
+$id = 'add_adr_'.$rod.$nadr;
+?>
+
+<p>
+<table border = 1>
+<tr>
+<td>
+Город
+<td>
+район
+<td>
+улица
+<td>
+дом
+<td>
+корпус
+<td>
+квартира
+<td>
+период
+<td>
+Не точно
+<tr>
+<td>
+<input type=text name = rod[mama][adres][<?php echo $nadr; ?>][gorod]>
+<td>
+<input type=text  name = rod[mama][adres][<?php echo $nadr; ?>][raion]>
+<td>
+<input type=text name = rod[mama][adres][<?php echo $nadr; ?>][ulica]>
+<td>
+<input type=text name = rod[mama][adres][<?php echo $nadr; ?>][dom]>
+<td>
+<input type=text name = rod[mama][adres][<?php echo $nadr; ?>][korpus]>
+<td>
+<input type=text name = rod[mama][adres][<?php echo $nadr; ?>][kv]>
+<td>
+<input type=text name = rod[mama][adres][<?php echo $nadr; ?>][period]>
+<td>
+<input type=checkbox name = rod[mama][adres][<?php echo $nadr; ?>][netochno]>
+</table>
+</div>
+
+<div class = add_adr_mama<?php echo $nadr; ?> id = add_adr_mama<?php echo $nadr; ?> >
+<button  type="button" onclick="ftc('add_adr.php?type=<?php echo $rod; ?>&nadr=<?php echo $nadr+1; ?>', '<?php echo $id; ?>');">
+	добавить еще одно место проживания
+</button>
