3bccda9e70a8e19e30738abbcbb1352ff7e95dbd
Ваше Имя <you@example.com>
2020-07-31 22:35:04 +0300

    1

--- 1.sh
+++ 1.sh
@@ -1,5 +1,6 @@
-for fname in mama.html  bab_m.html bab_p.html ded_m.html ded_p.html papa.html prabab_m.html
-do
-	cp $fname old
-done
-
+cp block_obe_8  block_mat_8
+cp block_obe_9  block_otc_9
+cp block_obe_4  block_mat_4
+cp block_obe_5  block_mat_5
+cp block_obe_6  block_otc_6
+cp block_obe_7  block_otc_7
---
+++ add_brak_pra.php
@@ -0,0 +1,48 @@
+<?php 
+$num = $_GET['num'];;
+$rod = $_GET['type'];
+$id = 'add_brak_'.$rod.$num;
+?>
+
+<p>
+<table>
+<tr>
+<td>
+ФИО супруга
+<td>
+<input type=text name = rod[<?php echo $rod; ?>][brak][<?php echo $num; ?>][fio][value]>
+<td>
+<input type=checkbox name = rod[<?php echo $rod; ?>][brak][<?php echo $num; ?>][fio][netochno]>
+<tr>
+<td>
+Дата заключения брака 
+<td>
+<input type=text name = rod[<?php echo $rod; ?>][brak][<?php echo $num; ?>][data][value]>
+<td>
+<input type=checkbox name = rod[<?php echo $rod; ?>][brak][<?php echo $num; ?>][data][netochno]>
+<tr>
+<td>
+Место заключения брака 
+<td>
+<input type=text name = rod[<?php echo $rod; ?>][brak][<?php echo $num; ?>][mesto][value]>
+<td>
+<input type=checkbox name = rod[<?php echo $rod; ?>][brak][<?php echo $num; ?>][mesto][netochno]>
+<tr>
+<td>
+Дата расторжения брака 
+<td>
+<input type=text name = rod[<?php echo $rod; ?>][brak][<?php echo $num; ?>][data_r][value]>
+<td>
+<input type=checkbox name = rod[<?php echo $rod; ?>][brak][<?php echo $num; ?>][data_r][netochno]>
+<tr>
+<td>
+</table>
+<?php endif; ?>
+
+</div>
+<div class = "<?php echo $id; ?>" id = "<?php echo $id; ?>" >
+
+<button  type="button" onclick="ftc('add_brak.php?type=<?php echo $rod; ?>&num=<?php echo $num+1; ?>', '<?php echo $id; ?>');">
+	добавить сведения о предыдущих браках
+</button>
+
