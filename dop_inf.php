1b6991aac304bbc546198c27387ff4ac0eac3576
Ваше Имя <you@example.com>
2020-08-07 23:01:35 +0300

    1

--- dop_inf.php
+++ dop_inf.php
@@ -1,4 +1,11 @@
+<?php 
+$rod = $_GET['type'];
+?>
 	Дополнительная информация
 <br>
 
-<textarea data = 'elastic' rows="2" cols="75" name = <?php echo $_GET['type']; ?>[dop][poisk][1] > </textarea>
+<div class="text">
+       <div class="comment_text_hidden"><div class="textarea_behavior" id="comment_text_hidden"></div></div>
+       <textarea  name = <?php echo $_GET['type']; ?>[dop] class="textarea_behavior" rows="2" id="comment_text" onkeyup="resizeArea('comment_text', 45, 450);"></textarea>
+</div>
+
